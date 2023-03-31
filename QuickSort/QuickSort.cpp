f7d542151e776ba8ad6c9e8e655ff9fe0b81f655
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-31 12:10:43 +0700

    cout << "\n-------------------" << endl;

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -16,3 +16,14 @@ void input() {
 		else
 			cout << "\n masukan panjang array adalah 20" << endl;
 	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
