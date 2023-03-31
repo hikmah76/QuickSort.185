1614c4a9bc7e5a7ab5e07a16ab0ae74965afb4cc
nur hikmah <nur.hikmah190504@gmail.com>
2023-03-31 15:14:49 +0700

    while (i <= j)//langkah

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -36,27 +36,44 @@ void swap(int x, int y) {
 }
 
 void q_short(int low, int high) {
-		int pivot, i, j;
-		if (low > high) // langkah 1
-			return;
-		// partition the list two parts
-		// one containing element less that or equal to pivot
-		// outher containing element greather than pivot 
-		i = low + 1;//langkah 3
-		j = high; //langkah 4
-		pivot = arr[low]; // langkah 2
+	int pivot, i, j;
+	if (low > high) // langkah 1
+		return;
+	// partition the list two parts
+	// one containing element less that or equal to pivot
+	// outher containing element greather than pivot 
+	i = low + 1;//langkah 3
+	j = high; //langkah 4
+	pivot = arr[low]; // langkah 2
 
-		while (i <= j)//langkah 10
+	while (i <= j)//langkah 10
+	{
+		//search for an element greather than pivot
+		while ((arr[i] <= pivot) && (i <= high))//langkah 5
 		{
-			//search for an element greather than pivot
-			while ((arr[i] <= pivot) && (i <= high))//langkah 5
-			{
-				i++;//langkah 6
-				cmp_count++;
-			}
-			cmp_count++;
-			//search for an element less thanor equal to pivot
-			while ((arr[j] > pivot) && (j >= low)) //langkah 7
-				j--;//langkah 8
+			i++;//langkah 6
 			cmp_count++;
 		}
+		cmp_count++;
+		//search for an element less thanor equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //langkah 7
+			j--;//langkah 8
+		cmp_count++;
+	}
+	cmp_count++;
+	if (i < j)//langkah 9
+		//if greater element is an left of teh element
+	{
+		//swap the element at index i with the element at index j
+		swap(i, j);
+		cmp_count++;
+	}
+	//sort the list an the left of pivot using quick sort
+	q_short(low, j - 1); //langkah 12
+	//sort the list an the right of pivot using quick sort
+	q_short(j + 1, high);//langkah 13
+}
+
+void display() {
+	cout << "\n-------------------------" << endl;
+	cout << "\n-------------------------" << endl;
